8ec09131b9849c71302450ace9884b1ea8f0edd2
DianaRenyao <857644432@qq.com>
2019-04-05 18:56:46 +0800

    message class for optional course

---
+++ src/main/java/buptspirit/spm/rest/message/OptionalCourseMessage.java
@@ -0,0 +1,63 @@
+package buptspirit.spm.rest.message;
+
+import buptspirit.spm.persistence.entity.UserInfoEntity;
+
+import java.sql.Date;
+
+public class OptionalCourseMessage {
+
+    private String courseName;
+    private String descriptionSummary;
+    private String teacherName;
+    private byte period;
+    private Date startTime;
+    private Date finishTime;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getDescriptionSummary() {
+        return descriptionSummary;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public byte getPeriod() {
+        return period;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public Date getFinishTime() {
+        return finishTime;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setDescriptionSummary(String descriptionSummary) {
+        this.descriptionSummary = descriptionSummary;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public void setPeriod(byte period) {
+        this.period = period;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setFinishTime(Date finishTime) {
+        this.finishTime = finishTime;
+    }
+}
