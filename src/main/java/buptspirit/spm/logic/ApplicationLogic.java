dff063061b967f88e6f85da9f314fc7c4c416e98
DianaRenyao <857644432@qq.com>
2019-04-07 20:30:55 +0800

    add return

--- src/main/java/buptspirit/spm/logic/ApplicationLogic.java
+++ src/main/java/buptspirit/spm/logic/ApplicationLogic.java
@@ -2,15 +2,37 @@ package buptspirit.spm.logic;
 
 import buptspirit.spm.exception.ServiceAssertionException;
 import buptspirit.spm.message.ApplicationCreationMessage;
+import buptspirit.spm.message.MessageMapper;
 import buptspirit.spm.message.SessionMessage;
 import buptspirit.spm.persistence.entity.ApplicationEntity;
+import buptspirit.spm.persistence.facade.ApplicationFacade;
+import buptspirit.spm.rest.filter.ApplicationState;
+
+import javax.inject.Inject;
+
+import static buptspirit.spm.persistence.JpaUtility.transactional;
 
 public class ApplicationLogic {
+    @Inject
+    ApplicationFacade applicationFacade;
+
+    @Inject
+    private MessageMapper messageMapper;
+
     public ApplicationCreationMessage createApplication(ApplicationCreationMessage applicationCreationMessage, SessionMessage sessionMessage) throws ServiceAssertionException {
         applicationCreationMessage.enforce();
-        ApplicationEntity applicationEntity= new ApplicationEntity();
-        applicationEntity.setComment(applicationCreationMessage.getComment());
-        applicationEntity.setCourseId(applicationCreationMessage.getCourseId());
-        applicationEntity.setState();
+        ApplicationEntity newApplication= new ApplicationEntity();
+        newApplication.setComment(applicationCreationMessage.getComment());
+        newApplication.setCourseId(applicationCreationMessage.getCourseId());
+        newApplication.setState(ApplicationState.Waiting.getState());
+        newApplication.setStudentUserId(sessionMessage.getUserInfo().getId());
+        return transactional(
+                em -> {
+                    newApplication.setStudentUserId(sessionMessage.getUserInfo().getId());
+                    applicationFacade.create(em, newApplication);
+                    return messageMapper.intoApplicationMessage(em, newApplication);
+                },
+                "failed to create course"
+        );
     }
 }
