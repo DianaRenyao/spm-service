b3a27707851dd864b52cf1192753dc12afb112ea
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-04-07 18:08:16 +0800

    add message resource

---
+++ src/main/java/buptspirit/spm/logic/MessageLogic.java
@@ -0,0 +1,87 @@
+package buptspirit.spm.logic;
+
+import buptspirit.spm.exception.ServiceAssertionException;
+import buptspirit.spm.message.MessageCreationMessage;
+import buptspirit.spm.message.MessageMessage;
+import buptspirit.spm.message.MessageWithRepliedMessage;
+import buptspirit.spm.message.PublicUserInfoMessage;
+import buptspirit.spm.message.SessionMessage;
+import buptspirit.spm.persistence.entity.MessageEntity;
+import buptspirit.spm.persistence.facade.MessageFacade;
+import buptspirit.spm.persistence.facade.UserInfoFacade;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static buptspirit.spm.persistence.JpaUtility.transactional;
+
+public class MessageLogic {
+
+    @Inject
+    private MessageFacade messageFacade;
+
+    @Inject
+    private UserInfoFacade userInfoFacade;
+
+    public List<MessageMessage> getAllMessage() {
+        return transactional(
+                em -> messageFacade.findAllWithAuthor(em)
+                        .map(MessageMessage::fromEntity)
+                        .collect(Collectors.toList()),
+                "failed to find messages"
+        );
+    }
+
+    public List<MessageWithRepliedMessage> getAllMessageWithReplied() {
+        return transactional(
+                em -> messageFacade.findAllWithAuthorAndReplied(em)
+                        .map(MessageWithRepliedMessage::fromEntity)
+                        .collect(Collectors.toList()),
+                "failed to find messages"
+        );
+    }
+
+    public List<MessageMessage> getAllMessageRanged(int first, int offset) {
+        return transactional(
+                em -> messageFacade.findAllWithAuthorRanged(em, first, offset)
+                        .map(MessageMessage::fromEntity)
+                        .collect(Collectors.toList()),
+                "failed to find messages"
+        );
+    }
+
+    public List<MessageWithRepliedMessage> getAllMessageWithRepliedRanged(int first, int offset) {
+        return transactional(
+                em -> messageFacade.findAllWithAuthorAndRepliedRanged(em, first, offset)
+                        .map(MessageWithRepliedMessage::fromEntity)
+                        .collect(Collectors.toList()),
+                "failed to find messages"
+        );
+    }
+
+    public MessageMessage createMessage(SessionMessage sessionMessage, MessageCreationMessage creationMessage)
+            throws ServiceAssertionException {
+        creationMessage.enforce();
+        MessageEntity entity = new MessageEntity();
+        entity.setAuthor(sessionMessage.getUserInfo().getId());
+        entity.setContent(creationMessage.getContent());
+        entity.setReplyTo(creationMessage.getReplyTo());
+        return transactional(
+                em -> {
+                    messageFacade.create(em, entity);
+                    PublicUserInfoMessage userInfoMessage = new PublicUserInfoMessage();
+                    userInfoMessage.setUsername(sessionMessage.getUserInfo().getUsername());
+                    userInfoMessage.setRealName(sessionMessage.getUserInfo().getRealName());
+                    MessageMessage message = new MessageMessage();
+                    message.setTimeCreated(entity.getTimeCreated());
+                    message.setContent(entity.getContent());
+                    message.setMessageId(entity.getMessageId());
+                    message.setAuthor(userInfoMessage);
+                    message.setReplyTo(entity.getReplyTo());
+                    return message;
+                },
+                "failed to create message "
+        );
+    }
+}
