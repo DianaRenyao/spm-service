430fbb7611df3be97e194ad03f2982e8b775d7c0
matrixlt <1322386221@qq.com>
2019-04-15 17:24:18 +0800

    change experiment error message place

--- src/main/java/buptspirit/spm/exception/ServiceError.java
+++ src/main/java/buptspirit/spm/exception/ServiceError.java
@@ -42,7 +42,7 @@ public enum ServiceError {
 
     /* course resource errors*/
     GET_COURSE_NO_SUCH_COURSE(60101, Response.Status.BAD_REQUEST, "no such course"),
-    POST_EXPERIMENT_NO_SUCH_COURSE(60201, Response.Status.BAD_REQUEST, "no such course"),
+
     //POST_COURSE_ALREADY_EXISTS(60201, Response.Status.BAD_REQUEST, "course already exists"), // course never already exists
 
     /* message resource error */
@@ -93,7 +93,10 @@ public enum ServiceError {
     /* exam error*/
     POST_EXAM_COURSE_DO_NOT_EXISTS(130301, Response.Status.BAD_REQUEST, "no such course"),
     POST_EXAM_CHAPTER_DO_NOT_EXISTS(130302, Response.Status.BAD_REQUEST, "no such chapter"),
-;
+
+    /*experiment error*/
+    POST_EXPERIMENT_NO_SUCH_COURSE(140201, Response.Status.BAD_REQUEST, "no such course"),
+    ;
 
 
     private final int code;
